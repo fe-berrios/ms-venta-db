5e67994542d5d497e24351a2be78e43397ad339b
fe-berrios <fe.berrios.ov@gmail.com>
2026-01-21 23:02:50 -0300

    primer commit

---
+++ src/main/java/bo/felipe/app/MsVentaDbApplication.java
@@ -0,0 +1,13 @@
+package bo.felipe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVentaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVentaDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/felipe/app/MsVentaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.felipe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVentaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
